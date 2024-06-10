c81df4903eb4002f8fde393b6df72a4f360cff4f
JeanSilva19 <117798106+JeanSilva19@users.noreply.github.com>
2024-06-10 14:37:21 -0400

    Add files via upload

---
+++ Projeto WEB (Ordem de serviço)/SITE DE ORDEM E SERVIÇO/Back-end/testeJEAN.php
@@ -0,0 +1,17 @@
+<?php
+$dbHost = 'Localhost';
+$dbUsername = 'root';
+$dbPassword = 'root';
+$dbName = 'Sigserv_Jean';
+
+$conexao = new mysqli($dbhosto,$dbUsername,$dbPassword,$dbName)
+
+// if($cibexao->connect_errno)
+// {
+// echo "Erro";
+// }
+// else{
+// echo"Rede Operacional!"
+// }
+
+?>
